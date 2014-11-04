de4b5fd89ca1c4063bf1f1fb345ee1c069131dcf
ark2812 <arnaud.cerckel@gmail.com>
2014-11-04 01:24:55 +0100

    voila ca compile pas trop au niveau des structures mais ma fonction pour calculer le centre du cercle marche ! :)

--- Code/main.c
+++ Code/main.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 
 
-void affiche(int *tableau, int tailleTableau);
- 
+
+//void affiche(int *tableau, int tailleTableau);
+double SetOfPointsCoordinates(double *table);
+ static double valOfPoints[6] = {0,0,0,1,1,1};
 int main(int argc, char *argv[])
 {
-    int tableau[4] = {10, 15, 3};
+    //int tableau[4] = {10, 15, 3};
  
-    // On affiche le contenu du tableau
-    affiche(tableau, 4);
+    //affiche(tableau, 4);
+    printf("%f\n",valOfPoints[3]);
+  double x =  SetOfPointsCoordinates(valOfPoints);
+   
+   printf("%f\n",x);
+   return 0;
  
-    return 0;
 }
  
 
--- Code/watson.c
+++ Code/watson.c
@@ -7,8 +7,29 @@
 #include <pthread.h>  
 #include <errno.h>
 
+// le pointeur vers les coordonnées de notre point
+typedef struct {
+    double x;
+    double y;
+} meshPoint;
 
-void affiche(int *tableau, int tailleTableau)
+typedef struct {
+    meshPoint *Point;
+} meshPoints;
+
+
+typedef struct {
+    meshPoints *Points;
+} meshProblem;
+
+meshPoint *meshPointCreate(double x, double y);
+meshPoints *meshPointsCreate(double *table);
+double findCenter(meshPoints* Points);
+//meshPoint *meshPointCreate(double x, double y);
+static meshPoints* ListPoints;
+
+// C example funcion 
+/*void affiche(int *tableau, int tailleTableau)
 {
     int i;
  
@@ -16,4 +37,83 @@ void affiche(int *tableau, int tailleTableau)
     {
         printf("%d\n", tableau[i]);
     }
+}*/
+
+double SetOfPointsCoordinates(double* table)
+{
+	printf("coucou");
+	meshPointsCreate(table);
+	double x = findCenter(ListPoints);
+	return x;
+
+}
+
+meshPoints *meshPointsCreate(double *table)
+{
+	int i;
+	
+	meshPoints* ListPoints = malloc(sizeof(meshPoints));
+	for (i = 0 ; i < 4 ; i++)
+    {
+        ListPoints->Point[i].x = table[2*i];
+        ListPoints->Point[i].y = table[2*i+1];
+    } 
+    return ListPoints;
+    
+}
+
+
+
+double findCenter(meshPoints* Points)
+{
+	double a,b,slopA, slopB, xCenter;//, yCenter;
+	double x1,x2,x3,y1,y2,y3;
+        x1 = ListPoints->Point[0].x;
+        printf("%f",x1);
+        y1 = ListPoints->Point[0].y;
+        x2 = ListPoints->Point[1].x;
+        y2 = ListPoints->Point[1].y;
+         x3 = ListPoints->Point[2].x;
+        y3 = ListPoints->Point[2].y;
+        
+
+	if (x2 == x1)
+	{
+		if(x3 == x1)
+		{
+			return -1;
+		}
+		a = x2;
+		b = y2;
+		x2 = x3;
+		y2 = y3;
+		x3 = a;
+		y3 = b;
+	
+	}
+	if (x3 == x2)
+	{
+		a = x2;
+		b = y2;
+		x2 = x1;
+		y2 = y1;
+		x1 = a;
+		y1 = b;
+	}
+	if(y3 == y1)
+	{
+			if(y3 == y2)
+			{
+			return -1;
+			}
+	}
+	printf("%f\n",x1);
+	slopA = (y2-y1)/(x2-x1);
+	printf("%f\n",slopA);
+	slopB = (y3-y2)/(x3-x2);
+	printf("%f\n",slopB);
+	xCenter = (slopA*slopB*(y1-y3) + slopB*(x1+x2) - slopA*(x2 + x3))/(2*(slopB-slopA));
+
+	//yCenter = -1/slopA*(xCenter - (x1+x2)/2) + (y1+y2)/2;
+	return xCenter;			
 }
