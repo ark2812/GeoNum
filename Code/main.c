f68843b5d9af544475ecaa78d5d7b72a0f8a7794
ark2812 <arnaud.cerckel@gmail.com>
2014-11-04 16:47:54 +0100

    voila ca dit si c'est à l'intérieur du cercle ou pas. Mtn je vais me mettre aussi a penser au structures.

--- Code/main.c
+++ Code/main.c
@@ -4,19 +4,18 @@
 
 
 //void affiche(int *tableau, int tailleTableau);
-double SetOfPointsCoordinates(double *table);
- static double valOfPoints[6] = {0,0,0,1,1,1};
+double isInside(double x1, double y1, double x2, double y2,double x3, double y3, double xR, double yR );
+//double SetOfPointsCoordinates(double *table);
+ //static double valOfPoints[6] = {0,0,0,1,1,1};
 int main(int argc, char *argv[])
 {
     //int tableau[4] = {10, 15, 3};
- 
-    //affiche(tableau, 4);
-    printf("%f\n",valOfPoints[3]);
-  double x =  SetOfPointsCoordinates(valOfPoints);
-   
-   printf("%f\n",x);
-   return 0;
- 
-}
- 
 
+    //affiche(tableau, 4);
+//    printf("%f\n",valOfPoints[3]);
+  //double x =  SetOfPointsCoordinates(valOfPoints);
+  int inside = isInside(0,0,0,1,1,1,0.6,0.4);
+   printf("%d\n",inside);
+   return 0;
+
+}
--- Code/watson.c
+++ Code/watson.c
@@ -4,9 +4,10 @@
 #include <math.h>
 #include <string.h>
 #include <GL/glfw.h>
-#include <pthread.h>  
+#include <pthread.h>
 #include <errno.h>
 
+/*
 // le pointeur vers les coordonnées de notre point
 typedef struct {
     double x;
@@ -28,16 +29,16 @@ double findCenter(meshPoints* Points);
 //meshPoint *meshPointCreate(double x, double y);
 static meshPoints* ListPoints;
 
-// C example funcion 
-/*void affiche(int *tableau, int tailleTableau)
+// C example funcion
+void affiche(int *tableau, int tailleTableau)
 {
     int i;
- 
+
     for (i = 0 ; i < tailleTableau ; i++)
     {
         printf("%d\n", tableau[i]);
     }
-}*/
+}
 
 double SetOfPointsCoordinates(double* table)
 {
@@ -51,15 +52,15 @@ double SetOfPointsCoordinates(double* table)
 meshPoints *meshPointsCreate(double *table)
 {
 	int i;
-	
+
 	meshPoints* ListPoints = malloc(sizeof(meshPoints));
 	for (i = 0 ; i < 4 ; i++)
     {
         ListPoints->Point[i].x = table[2*i];
         ListPoints->Point[i].y = table[2*i+1];
-    } 
+    }
     return ListPoints;
-    
+
 }
 
 
@@ -75,7 +76,7 @@ double findCenter(meshPoints* Points)
         y2 = ListPoints->Point[1].y;
          x3 = ListPoints->Point[2].x;
         y3 = ListPoints->Point[2].y;
-        
+
 
 	if (x2 == x1)
 	{
@@ -89,7 +90,7 @@ double findCenter(meshPoints* Points)
 		y2 = y3;
 		x3 = a;
 		y3 = b;
-	
+
 	}
 	if (x3 == x2)
 	{
@@ -115,5 +116,60 @@ double findCenter(meshPoints* Points)
 	xCenter = (slopA*slopB*(y1-y3) + slopB*(x1+x2) - slopA*(x2 + x3))/(2*(slopB-slopA));
 
 	//yCenter = -1/slopA*(xCenter - (x1+x2)/2) + (y1+y2)/2;
-	return xCenter;			
+	return xCenter;
+}*/
+
+double isInside( double x1, double y1, double x2, double y2,double x3, double y3, double xR, double yR )
+{
+    double a, b, slopA, slopB, xCenter, yCenter;
+  if (x2 == x1)
+  {
+    if(x3 == x1)
+    {
+      return -1;
+    }
+    a = x2;
+    b = y2;
+    x2 = x3;
+    y2 = y3;
+    x3 = a;
+    y3 = b;
+
+  }
+  if (x3 == x2)
+  {
+    a = x2;
+    b = y2;
+    x2 = x1;
+    y2 = y1;
+    x1 = a;
+    y1 = b;
+  }
+  if(y3 == y1)
+  {
+      if(y3 == y2)
+      {
+      return -1;
+      }
+  }
+  printf("%f\n",x1);
+  slopA = (y2-y1)/(x2-x1);
+  printf("%f\n",slopA);
+  slopB = (y3-y2)/(x3-x2);
+  printf("%f\n",slopB);
+  xCenter = (slopA*slopB*(y1-y3) + slopB*(x1+x2) - slopA*(x2 + x3))/(2*(slopB-slopA));
+  yCenter = -1/slopA*(xCenter - (x1+x2)/2) + (y1+y2)/2;
+
+  double radius; int inside;
+  radius = sqrt((x1-xCenter)*(x1-xCenter)+(y1-yCenter)*(y1-yCenter));
+  if((xR-xCenter)*(xR-xCenter)+(yR-yCenter)*(yR-yCenter)<=radius*radius)
+    {
+        inside = 1;
+    }
+    else
+      {
+        inside =0;
+      }
+
+  return inside ;
 }
